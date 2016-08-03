a55c19bed524b6b1392a3adb76b6c1b976a68cef
Kimberly Arvin <karvin@e1z1r5p37.42.us.org>
2016-08-02 17:46:36 -0700

    yes

---
+++ first_word/first_word.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   first_word.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: karvin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/08/02 16:17:24 by karvin            #+#    #+#             */
+/*   Updated: 2016/08/02 17:37:46 by karvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	first_word(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t')
+		i++;
+	while (str[i] != '\0' && str[i] != ' ' && str[i] != '\t')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc == 2)
+		first_word(argv[1]);
+	ft_putchar('\n');
+	return(0);
+}
